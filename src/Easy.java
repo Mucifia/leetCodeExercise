e944c63ed4c2c71a5f58e5097ecf1ce796673d71
moumy <mmy29279@outlook.com>
2018-02-06 18:22:20 +0800

    Reverse a 32bit Interger

--- src/Easy.java
+++ src/Easy.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 public class Easy {
 
     /**
@@ -13,6 +15,36 @@ public class Easy {
                     return new int[]{i,j};
         return null;
     }
-    public static void main(String[] args){
+
+    /**
+     * reverse a  32 bit interger
+     * attention : must consider the range of INTERGER
+     * or your operation may overflow
+     * @param x
+     * @return
+     */
+    public int reverse(int x) {
+        int temp = x > 0  ? x :-(1*x);
+        int end  = temp%10;
+        int reversed =0;
+        while (temp!=0){
+            if(reversed >Integer.MAX_VALUE/10||reversed<Integer.MIN_VALUE/10){
+                return 0;
+            }
+            reversed=reversed*10+end;
+            temp=temp/10;
+            end=temp%10;
+        }
+
+        return reversed*(x>0 ? 1 : -1);
     }
+
+    @Test
+    public void test(){
+        int a = reverse(-2147483648);
+        int b = reverse(-123);
+        int c = reverse(120);
+        int d = Integer.MAX_VALUE;
+    }
+
 }
