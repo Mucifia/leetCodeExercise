430b7b81305d2a5a770705434d05c7a7770cd06c
moumy <mmy29279@outlook.com>
2018-06-10 20:27:58 +0800

    mergeTwoLists

--- src/com/mmy/leetcode/easy/Easy2.java
+++ src/com/mmy/leetcode/easy/Easy2.java
@@ -38,8 +38,43 @@ public class Easy2 {
     return stack.empty();
   }
 
+
+  public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    ListNode head = new ListNode(-1) ;
+    ListNode copy=head;
+    while (l1!=null&&l2!=null){
+      if (l1.val>=l2.val){
+        head.next=l2;
+        l2=l2.next;
+        head=head.next;
+      }else {
+        head.next=l1;
+        l1=l1.next;
+        head=head.next;
+      }
+    }
+    if (l1==null){
+      head.next=l2;
+    }
+    if (l2==null){
+      head.next=l1;
+    }
+    return copy.next;
+  }
+
+
   @Test
   public void test() {
     isValid("}");
+  }
+}
+
+class ListNode {
+
+  int val;
+  ListNode next;
+
+  ListNode(int x) {
+    val = x;
   }
 }
