22623f0f3a75f0fb46973ecd864596612fda4862
moumy <mmy29279@outlook.com>
2018-06-13 14:44:55 +0800

    swapPairs
    Signed-off-by: moumy <mmy29279@outlook.com>

--- src/com/mmy/leetcode/medium/Medium4.java
+++ src/com/mmy/leetcode/medium/Medium4.java
@@ -1,12 +1,17 @@
 package com.mmy.leetcode.medium;
 
 
+import java.util.ArrayDeque;
+import java.util.Queue;
+import org.junit.jupiter.api.Test;
+
 /**
  * @author: mmy
  * @date: 2018/06/12
  * @description:
  */
 public class Medium4 {
+
   public class ListNode {
 
     int val;
@@ -20,12 +25,36 @@ public class Medium4 {
   /**
    * Given a linked list, swap every two adjacent nodes and return its head.
    *
-   * Node:Your algorithm should use only constant extra space.
-   *      You may not modify the values in the list's nodes, only nodes itself may be changed.
-   * @param head
-   * @return
+   * Node:Your algorithm should use only constant extra space. You may not modify the values in the
+   * list's nodes, only nodes itself may be changed.
    */
   public ListNode swapPairs(ListNode head) {
-    return null;
+    Queue<ListNode> queue = new ArrayDeque<>(2);
+    ListNode newHead = new ListNode(-1);
+    newHead.next = head;
+    ListNode copy = newHead;
+    while (newHead != null) {
+      if (queue.size() < 2) {
+        queue.offer(newHead);
+      } else {
+        ListNode first = queue.poll();
+        ListNode second = queue.poll();
+        second.next = newHead.next;
+        newHead.next = second;
+        first.next = newHead;
+        queue.offer(second);
+        newHead = second;
+      }
+      newHead=newHead.next;
+    }
+    return copy.next;
+  }
+
+  @Test
+  public void test() {
+    ListNode s = swapPairs(null);
+
   }
 }
+
+
