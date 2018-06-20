c6d0c9a9b49ae4d2b4e802d334e4789496cc6d8a
moumy <mmy29279@outlook.com>
2018-06-20 09:12:14 +0800

    removeElement && strStr

--- src/com/mmy/leetcode/easy/Easy2.java
+++ src/com/mmy/leetcode/easy/Easy2.java
@@ -40,24 +40,24 @@ public class Easy2 {
 
 
   public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-    ListNode head = new ListNode(-1) ;
-    ListNode copy=head;
-    while (l1!=null&&l2!=null){
-      if (l1.val>=l2.val){
-        head.next=l2;
-        l2=l2.next;
-        head=head.next;
-      }else {
-        head.next=l1;
-        l1=l1.next;
-        head=head.next;
+    ListNode head = new ListNode(-1);
+    ListNode copy = head;
+    while (l1 != null && l2 != null) {
+      if (l1.val >= l2.val) {
+        head.next = l2;
+        l2 = l2.next;
+        head = head.next;
+      } else {
+        head.next = l1;
+        l1 = l1.next;
+        head = head.next;
       }
     }
-    if (l1==null){
-      head.next=l2;
+    if (l1 == null) {
+      head.next = l2;
     }
-    if (l2==null){
-      head.next=l1;
+    if (l2 == null) {
+      head.next = l1;
     }
     return copy.next;
   }
@@ -65,55 +65,120 @@ public class Easy2 {
 
   /**
    * 第一次思路是，元素逐渐左移，最后多余操作过多
-   * @param nums
-   * @return
    */
   public int removeDuplicates(int[] nums) {
-    int index =0;
-    int count=0;
-    for (int i =1;i<nums.length;){
+    int index = 0;
+    int count = 0;
+    for (int i = 1; i < nums.length; ) {
 
-      if (nums[i]==nums[index]){
-        if (count>nums.length-index){
+      if (nums[i] == nums[index]) {
+        if (count > nums.length - index) {
           break;
         }
-        for (int j =i;j<nums.length-1;j++){
-          nums[j]=nums[j+1];
+        for (int j = i; j < nums.length - 1; j++) {
+          nums[j] = nums[j + 1];
         }
         count++;
-      }else {
-        index=i;
+      } else {
+        index = i;
         i++;
-        count=0;
+        count = 0;
       }
     }
-    return index+1;
+    return index + 1;
   }
 
   /**
    * 看了网上的优秀答案，启发，直接用不同值替换前n个即可，学到了
-   * @param nums
-   * @return
    */
-  public int removeDuplicates2(int[] nums){
-    if (nums.length==0){
+  public int removeDuplicates2(int[] nums) {
+    if (nums.length == 0) {
       return 0;
     }
-    int size=1;
-    int num =nums[0];
-    for (int i =1;i<nums.length;i++){
-      if (num==nums[i]){
+    int size = 1;
+    int num = nums[0];
+    for (int i = 1; i < nums.length; i++) {
+      if (num == nums[i]) {
         continue;
       }
-      nums[size++]=nums[i];
-      num=nums[i];
+      nums[size++] = nums[i];
+      num = nums[i];
     }
     return size;
   }
 
+
+  /**
+   * Given an array nums and a value val, remove all instances of that value in-place and return the
+   * new length. 学以致用，嘻嘻
+   */
+  public int removeElement(int[] nums, int val) {
+    int size = 0;
+    for (int i = 0; i < nums.length; i++) {
+      if (nums[i] != val) {
+        nums[size++] = nums[i];
+      }
+    }
+    return size;
+  }
+
+  /**
+   * 原本通过遍历可以得到，但是突然想到了kmp算法，就将其应用到此处
+   * @param haystack
+   * @param needle
+   * @return
+   */
+  public int strStr(String haystack, String needle) {
+    if (needle.length() == 0 || needle.length() == 0) {
+      return 0;
+    }
+    int[] next = new int[needle.length()];
+    getNextTable(needle, next);
+    int k = -1;
+    for (int i = 0; i < haystack.length(); i++) {
+      /**
+       *此处应该理解为，到haystatck第i个的时候，needle匹配到了第k个  `
+       */
+      while (k > -1 && needle.charAt(k + 1) != haystack.charAt(i))//ptr和str不匹配，且k>-1（表示ptr和str有部分匹配）
+      {
+        k = next[k];//往前回溯
+      }
+      if (needle.charAt(k + 1) == haystack.charAt(i)) {
+        k = k + 1;
+      }
+      if (k == needle.length() - 1)//说明k移动到ptr的最末端
+      {
+        return i - needle.length() + 1;//返回相应的位置
+      }
+    }
+    return -1;
+  }
+
+  public void getNextTable(String string, int[] next) {
+    int len = string.length();
+    int k = -1;
+    next[0] = -1;
+    for (int q = 1; q < len; q++) {
+      //进入循环后，k为next[q-1]的最长相同前后缀
+      //下面比较为对相同前后缀补位，若补位后值，则next[q]的最长前后缀值为next[q-1]+1
+      //若补位后不同，则寻找next[q-1]的最小前后缀，进行补位，重复匹配
+      //若仔细模拟一遍，发现完全符合道理！有一部分动态规划的思想在其中。
+      while (k > -1 && string.charAt(k + 1) != string.charAt(q)) {
+        k = next[k];
+      }
+
+      if (string.charAt(k + 1) == string.charAt(q)) {
+        k = k + 1;
+      }
+      next[q] = k;
+    }
+  }
+
+
   @Test
   public void test() {
-    int j =removeDuplicates2(new int[]{1,1,2});
+    String a = "bacbababadababacambabacaddababacasdsd";
+    int index = strStr(a, "ababaca");
   }
 }
 
