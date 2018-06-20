28b701e6641c255a5e8e7122ef4e9ad08594c005
moumy <mmy29279@outlook.com>
2018-06-20 16:31:10 +0800

    divide

--- src/com/mmy/leetcode/medium/Medium4.java
+++ src/com/mmy/leetcode/medium/Medium4.java
@@ -50,10 +50,51 @@ public class Medium4 {
     return copy.next;
   }
 
+  /**
+   * Given two integers dividend and divisor, divide two integers without using multiplication, division and mod operator.
+   *
+   * Return the quotient after dividing dividend by divisor.
+   *
+   * The integer division should truncate toward zero.
+   *
+   * hint: we can use bit calculation or use a int to record times
+   * @param dividend
+   * @param divisor
+   * @return
+   */
+  public int divide(int dividend, int divisor) {
+    if (dividend==Integer.MIN_VALUE&&divisor==-1){
+      return Integer.MAX_VALUE;
+    }
+    //key point: make Integer.MIN_VALUE reverse not overflow
+    long tempDividend=Math.abs((long)dividend);
+    long tempDivisor=Math.abs((long)divisor);
+    int result=0;
+    int times;
+    long divisorCopy=tempDivisor;
+    boolean beyondZero = (dividend>0&&divisor<0)||(dividend<0&&divisor>0)?false:true;
+    while (tempDividend>tempDivisor){
+      divisorCopy=tempDivisor;
+      times = 1;
+      while (tempDividend>=divisorCopy<<1){
+        divisorCopy = divisorCopy<<1;
+        times = times <<1;
+        //also can change to divisorCopy +=divisorCopy
+        //times +=time
+      }
+      tempDividend = tempDividend-divisorCopy;
+      result=result+times;
+    }
+    if (tempDividend==tempDivisor){
+      result++;
+    }
+    return beyondZero?result:-1*result;
+  }
+
+
   @Test
   public void test() {
-    ListNode s = swapPairs(null);
-
+    int a  = divide(-2147483648,1);
   }
 }
 
