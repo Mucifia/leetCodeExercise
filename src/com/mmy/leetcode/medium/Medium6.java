1764bb1f5f51133f40de74bf1c7ffb6216aff7c5
moumy <mmy29279@outlook.com>
2018-07-16 19:13:30 +0800

    40. Combination Sum II

--- src/com/mmy/leetcode/medium/Medium6.java
+++ src/com/mmy/leetcode/medium/Medium6.java
@@ -7,6 +7,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Queue;
 import java.util.Set;
 import org.junit.Test;
 
@@ -91,9 +92,70 @@ public class Medium6 {
     return false;
   }
 
+  /**
+   * can't get dulplicate candidate
+   * @param candidates
+   * @param target
+   * @return
+   */
+  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+    Arrays.sort(candidates);
+    List<Integer> candidatesList = new ArrayList<>();
+    Set lists = new HashSet();
+    for (int i : candidates){
+      candidatesList.add(i);
+    }
+    searchCandidates(lists,new ArrayList<>(),target,candidatesList,0);
+    return new ArrayList<>(lists);
+  }
+
+  public boolean searchCandidates(Set lists,List<Integer> list,int target,List<Integer> candidates,int index){
+    if (target>0){
+      for (int i=index;i<candidates.size()&&candidates.get(i)<=target;i++){
+        List<Integer> listCopy =new ArrayList<>(list);
+        int num = candidates.get(i);
+        listCopy.add(num);
+        candidates.remove(i);
+        if (searchCandidates(lists,listCopy,target-num,new ArrayList<>(candidates),i)){
+          lists.add(listCopy);
+        }else {
+          listCopy.remove(listCopy.size()-1);
+          candidates.add(i,num);
+        }
+      }
+    }else if (target==0) {
+      return true;
+    }
+      return false;
+
+  }
+//  public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+//    Arrays.sort(candidates);
+//    List<List<Integer>> results = new ArrayList<>();
+//    backtracking (results, new ArrayList<>(), candidates, target, 0);
+//    return results;
+//  }
+  //网上最快答案
+//  private void backtracking(List<List<Integer>> results, List<Integer> temp, int[]candidates, int target, int start){
+//    if (target == 0){
+//      results.add (new ArrayList<>(temp));
+//      return;
+//    }
+//    for (int i=start; i<candidates.length && candidates[i]<=target; i++){
+//      if (i>start && candidates[i]==candidates[i-1]){
+//        continue;
+//      }
+//      temp.add(candidates[i]);
+//      backtracking(results, temp, candidates, target-candidates[i], i+1);
+//      temp.remove(temp.size()-1);
+//    }
+//
+//  }
+
+
   @Test
   public void test(){
     int[] nums=  new int[]{1,2,3,6,7};
-    List<List<Integer>> lists =combinationSum(nums,7);
+    List<List<Integer>> lists =combinationSum2(nums,7);
   }
 }
