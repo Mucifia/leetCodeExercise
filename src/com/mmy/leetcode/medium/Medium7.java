e8f5c97998237dc6b22fbf6e07be2247e8a40a30
moumy <mmy29279@outlook.com>
2018-07-25 21:50:46 +0800

    49. Group Anagrams

--- src/com/mmy/leetcode/medium/Medium7.java
+++ src/com/mmy/leetcode/medium/Medium7.java
@@ -1,5 +1,11 @@
 package com.mmy.leetcode.medium;
 
+import com.mmy.leetcode.medium.Medium1.ListNode;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.junit.Test;
 
 /**
@@ -35,14 +41,57 @@ public class Medium7 {
     matrix[m][n] = t;
   }
 
+
+  /**
+   * Given an array of strings, group anagrams together.
+   * Input: ["eat", "tea", "tan", "ate", "nat", "bat"],
+   * Output:
+   * [
+   *   ["ate","eat","tea"],
+   *   ["nat","tan"],
+   *   ["bat"]
+   * ]
+   * @param strs
+   * @return
+   */
+  public List<List<String>> groupAnagrams(String[] strs) {
+    Map<String,List<String>> hashmap = new HashMap<>();
+    for (String s : strs){
+      char[] c = s.toCharArray();
+      Arrays.sort(c);
+      String str = String.valueOf(c);
+      if (hashmap.get(str)==null){
+        List<String> list = new ArrayList<>();
+        list.add(s);
+        hashmap.put(str,list);
+      }else {
+        hashmap.get(str).add(s);
+      }
+    }
+    return new ArrayList<>(hashmap.values());
+  }
+
+
+
+
+
+
+
+
+
+
   @Test
   public void test() {
-    rotate(new int[][]{
-        {5, 1, 9, 11},
-        {2, 4, 8, 10},
-        {13, 3, 6, 7},
-        {15, 14, 12, 16}
-    });
+//    rotate(new int[][]{
+//        {5, 1, 9, 11},
+//        {2, 4, 8, 10},
+//        {13, 3, 6, 7},
+//        {15, 14, 12, 16}
+//    });
+    String[] s  = new String[]{"eat", "tea", "tan", "ate", "nat", "bat"};
+    List<List<String>> lists = groupAnagrams(s);
+
+
   }
 
 }
