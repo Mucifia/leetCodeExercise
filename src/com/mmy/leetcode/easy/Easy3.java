edb56b08a8609518997ae54ee8ddffbfb229d7e6
moumy <mmy29279@outlook.com>
2018-08-04 17:31:06 +0800

    58. Length of Last Word

--- src/com/mmy/leetcode/easy/Easy3.java
+++ src/com/mmy/leetcode/easy/Easy3.java
@@ -42,11 +42,6 @@ public class Easy3 {
     return s.toString();
   }
 
-  @Test
-  public  void test(){
-    int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
-    int result = maxSubArray(nums);
-  }
 
 
   public int maxSubArray(int[] nums) {
@@ -61,6 +56,49 @@ public class Easy3 {
       }
     }
     return result;
+  }
+
+
+
+
+  //58. Length of Last Word
+  /**
+   *Given a string s consists of upper/lower-case alphabets and empty space characters ' ', return the length of last word in the string.
+    If the last word does not exist, return 0.
+    Note: A word is defined as a character sequence consists of non-space characters only.
+   *
+   * @param s
+   * @return
+   */
+  public int lengthOfLastWord(String s) {
+    boolean isNewWord = false;
+    int length = 0;
+    int lastLength=length;
+    char[] c  = s.toLowerCase().toCharArray();
+    for (int i=0;i<c.length;i++){
+      if (c[i]>='a'&&c[i]<='z'&&isNewWord){
+        length++;
+        lastLength=length;
+      }else if (c[i]<'a'||c[i]>'z'){
+        isNewWord = false;
+        length=0;
+      }else if (c[i]>='a'&&c[i]<='z'&&!isNewWord){
+        length = 1;
+        isNewWord = true;
+        lastLength=length;
+      }
+    }
+    return lastLength;
+  }
+
+
+  @Test
+  public  void test(){
+//    int[] nums = new int[]{-2,1,-3,4,-1,2,1,-5,4};
+//    int result = maxSubArray(nums);
+    String s = "Hello World   ";
+    int result =  lengthOfLastWord(s);
+
   }
 
 
