a0c86384e3b3b7a114db1a3971d84431721a7a38
moumy <mmy29279@outlook.com>
2018-08-10 13:01:14 +0800

    64	Minimum Path Sum

--- src/com/mmy/leetcode/medium/Medium9.java
+++ src/com/mmy/leetcode/medium/Medium9.java
@@ -44,6 +44,30 @@ public class Medium9 {
     return map[m-1][n-1];
   }
 
+
+  public int minPathSum(int[][] grid) {
+    int m = grid.length;
+    int n = grid[0].length;
+    if (m==0||n==0){
+      return 0;
+    }
+    int[][] map = new int[m][n];
+    map[0][0]=grid[0][0];
+    for (int i =1;i<m;i++){
+      map[i][0]=grid[i][0]+map[i-1][0];
+    }
+    for (int j=1;j<n;j++){
+      map[0][j]=grid[0][j]+map[0][j-1];
+    }
+    for (int i=1;i<m;i++){
+      for (int j=1;j<n;j++){
+        int min = Math.min(map[i-1][j],map[i][j-1]);
+        map[i][j]=min+grid[i][j];
+      }
+    }
+    return map[m-1][n-1];
+  }
+
   @Test
   public void test(){
     int[][] obstacles = new int[][]{{0,0,0},{0,1,0},{0,0,0}};
