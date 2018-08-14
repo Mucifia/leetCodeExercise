d3b08ac82c3e83bf795ec798e3b04c63fde744c2
moumy <mmy29279@outlook.com>
2018-08-14 09:08:35 +0800

    69 Sqrt(x)

--- src/com/mmy/leetcode/easy/Easy3.java
+++ src/com/mmy/leetcode/easy/Easy3.java
@@ -178,7 +178,55 @@ public class Easy3 {
   }
 
 
+  /**
+   * Implement int sqrt(int x).
+   *
+   * Compute and return the square root of x, where x is guaranteed to be a non-negative integer.
+   *
+   * Since the return type is an integer, the decimal digits are truncated and only the integer part of the result is returned.
+   * @param x
+   * @return
+   */
+  public int mySqrt(int x) {
+    if (x==0){
+      return 0;
+    }
+    int result =1;
+    while (result<=x/result){
+      result++;
+    }
+    return result-1;
+  }
 
+  /**
+   * You are climbing a stair case. It takes n steps to reach to the top.
+   *
+   * Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
+   *
+   * Note: Given n will be a positive integer.
+   * @param n
+   * @return
+   * TLS
+   */
+  public int climbStairs(int n) {
+    if (n==1){
+      return 1;
+    }else if (n==2){
+      return 2;
+    }
+    return climbStairs(n-1)+climbStairs(n-2);
+  }
+
+
+  public int climbStairs2(int n) {
+    int[] result = new int[n];
+    result[0]=0;
+    result[1]=1;
+    for (int i=2;i<n;i++){
+      result[i]=result[i-1]+result[i-2];
+    }
+    return result[n-1];
+  }
 
   @Test
   public  void test(){
