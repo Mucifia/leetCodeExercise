697894487f6ff40b58ee07507eb379797fac58cc
moumy <mmy29279@outlook.com>
2018-09-02 23:32:40 +0800

    81 Search in Rotated Sorted Array II

--- src/com/mmy/leetcode/medium/Medium11.java
+++ src/com/mmy/leetcode/medium/Medium11.java
@@ -94,12 +94,82 @@ public class Medium11 {
     }
     return len+1;
   }
+
+
+  public boolean search(int[] nums, int target) {
+    if (nums.length==0){
+      return false;
+    }
+    int pivot = 0;
+    while (pivot<nums.length-1){
+      if (nums[pivot+1]>=nums[pivot]){
+        pivot++;
+      }else {
+        break;
+      }
+    }
+    return binarySearch(0,pivot,nums,target)
+        ||binarySearch(pivot+1,nums.length-1,nums,target);
+  }
+
+  boolean binarySearch(int left,int right,int[] nums,int target){
+    int l= left;
+    int r = right;
+    while (l<=r){
+      int m = (l+r)/2;
+      if (nums[m]==target){
+        return true;
+      }else if (nums[m]<target){
+        return binarySearch(m+1,r,nums,target);
+      }else {
+        return  binarySearch(l,m-1,nums,target);
+      }
+    }
+    return false;
+  }
+
+
+  /**
+   * one time binarySearch
+   * cost 0ms
+   * @param nums
+   * @param target
+   * @return
+   */
+  public boolean search2(int[] nums, int target) {
+    if(nums.length == 0) return false;
+    int start = 0, end = nums.length - 1;
+    while(start < end){
+      int mid = start + (end - start) / 2;
+      if(nums[mid] == target) return true;
+      if(nums[mid] > nums[start]){
+        if(nums[mid] > target && nums[start] <= target){
+          end = mid - 1;
+        }else{
+          start = mid + 1;
+        }
+      }else if(nums[mid] < nums[start]){
+        if(nums[mid] < target && nums[end] >= target){
+          start = mid + 1;
+        }else{
+          end = mid - 1;
+        }
+      }else{
+        start ++;
+      }
+    }
+    return nums[start] == target;
+  }
+
   @Test
   public void test(){
-    int[] test1 = new int[]{1,1,1,2,2,3};
-    int[] test2 = new int[]{0,0,1,1,1,1,2,3,3};
-    int res1= removeDuplicates(test1);
-    int res2 =removeDuplicates(test2);
+//    int[] test1 = new int[]{1,1,1,2,2,3};
+//    int[] test2 = new int[]{0,0,1,1,1,1,2,3,3};
+//    int res1= removeDuplicates(test1);
+//    int res2 =removeDuplicates(test2);
+    int[] test= new int[]{3,1};
+    int target= 0;
+    boolean res = search(test,target);
   }
 
 }
